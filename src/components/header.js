185934e480bb93db1ff61f5d6cf61be0546586d4
kedorafr <alexis@kedora.fr>
2019-07-11 15:09:43 +0200

    change header

--- src/components/header.js
+++ src/components/header.js
@@ -5,7 +5,7 @@ import React from "react"
 const Header = ({ siteTitle }) => (
   <nav className="navbar" role="navigation" aria-label="main navigation">
     <div className="navbar-item">
-      <Link to="/" className="subtitle" > 💳 Stripe Element with Gatsby.js</Link>
+      <Link to="/" className="subtitle" > 💳 | Stripe Element with Gatsby.js</Link>
     </div>
     <div className="navbar-end">
       <div className="navbar-item">
